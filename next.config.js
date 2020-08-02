ff8d4297a1b9fa5c76b4a2ac5ecb0a7fbf314a8b
utgwkk <utagawakiki@gmail.com>
2020-08-02 17:09:45 +0900

    eslint --fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-const { getRedirectStatus } = require("next/dist/lib/load-custom-routes");
-
 module.exports = {
-    async redirects() {
-        return [
-            {
-                source: '/labs/:path*',
-                destination: 'https://sugarheart.utgw.net/labs/:path*',
-                permanent: true,
-            }
-        ];
-    },
+  async redirects() {
+    return [
+      {
+        source: "/labs/:path*",
+        destination: "https://sugarheart.utgw.net/labs/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
