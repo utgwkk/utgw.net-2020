3f820f963708c1043a0141b01de72daad4cf5137
utgwkk <utagawakiki@gmail.com>
2020-08-02 17:32:06 +0900

    charSet

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
