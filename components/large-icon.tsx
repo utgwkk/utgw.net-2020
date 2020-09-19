24fb628765f699517029ef36ada860143329f401
utgwkk <utagawakiki@gmail.com>
2020-09-19 01:39:59 +0000

    Revert "centerize icon with <img> tag"
    This reverts commit ecad9afcb8bc7a182a38aa3496ad096a1d1c4fc5.

--- components/large-icon.tsx
+++ components/large-icon.tsx
@@ -1,3 +1,3 @@
 import React from "react";
 
-export const LargeIcon: React.FC = () => <div className='icon-placeholder'><img src='/img/nikutai.png' id='icon' /></div>;
+export const LargeIcon: React.FC = () => <div id="icon" />;
