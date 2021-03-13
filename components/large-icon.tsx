ed49c0f2034e1a8eb4181730322092e9732e8288
utgwkk <utagawakiki@gmail.com>
2021-03-13 11:41:51 +0900

    large icon with <Image>

--- components/large-icon.tsx
+++ components/large-icon.tsx
@@ -1,3 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
-export const LargeIcon: React.VFC = () => <div id="icon" />;
+export const LargeIcon: React.VFC = () => (
+  <div className="icon-container">
+    <Image
+      src="/img/nikutai.png"
+      className="icon"
+      width={500}
+      height={500}
+      alt="icon"
+    />
+  </div>
+);
