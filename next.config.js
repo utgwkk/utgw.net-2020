e061e86a9bce798ac2346da8e3a3bdb244a3ea57
utgwkk <utagawakiki@gmail.com>
2023-07-12 08:41:21 +0900

    remove redirects

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   output: "export",
-  async redirects() {
-    return [
-      {
-        source: "/labs/:path*",
-        destination: "https://sugarheart.utgw.net/labs/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
