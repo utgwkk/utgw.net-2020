7b66904792bfbbbaa28181e40bceb1b09c87454d
utgwkk <utagawakiki@gmail.com>
2023-07-12 08:46:22 +0900

    Revert "remove redirects"
    This reverts commit e061e86a9bce798ac2346da8e3a3bdb244a3ea57.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   output: "export",
+  async redirects() {
+    return [
+      {
+        source: "/labs/:path*",
+        destination: "https://sugarheart.utgw.net/labs/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
