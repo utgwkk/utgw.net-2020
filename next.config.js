7c85f7de03ee7f085fb0cbdfebcfa1d55cd14e81
utgwkk <utagawakiki@gmail.com>
2024-11-25 15:00:11 +0900

    cache redirect for /labs

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-async function redirects() {
-  return [
-    {
-      source: "/labs/:path*",
-      destination: "https://sugarheart.utgw.net/labs/:path*",
-      permanent: true,
-    },
-  ];
-}
-
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      redirects,
-    };
+    return {};
   }
   return {
-    redirects,
     output: "standalone",
     distDir: "dist",
   };
