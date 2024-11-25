ee4bcc8c608cac7b019c60bef19d795da104e32a
utgwkk <utagawakiki@gmail.com>
2024-11-25 14:24:58 +0900

    enable redirects

--- next.config.js
+++ next.config.js
@@ -1,23 +1,26 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
+async function redirects() {
+  return [
+    {
+      source: "/labs/:path*",
+      destination: "https://sugarheart.utgw.net/labs/:path*",
+      permanent: true,
+    },
+  ];
+}
+
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
-      async redirects() {
-        return [
-          {
-            source: "/labs/:path*",
-            destination: "https://sugarheart.utgw.net/labs/:path*",
-            permanent: true,
-          },
-        ];
-      },
+      redirects,
     };
   }
   return {
+    redirects,
     output: "standalone",
     distDir: "dist",
   };
