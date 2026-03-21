1a1aad7a0880097270849a7e441d72607ddcfc05
UTAGAWA Kiki <utagawakiki@gmail.com>
2026-03-21 13:06:00 +0900

    リダイレクトのテスト

---
+++ e2e/redirect.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+
+test("/labs 以下のパスは https://sugarheart.utgw.net/labs にリダイレクトする", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/labs/tatsuya.htm");
+
+  await expect(page).toHaveURL("https://sugarheart.utgw.net/labs/tatsuya.htm");
+});
